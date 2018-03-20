06beb8d25911734f46fe0d466de5b29c4b180194
xiaofeng <1205381116@qq.com>
2018-03-20 23:44:29 +0800

    wormhole

---
+++ wormhole/wormhole.py
@@ -0,0 +1,68 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ID: xiaofen6
+LANG: PYTHON2
+PROG: wormhole
+"""
+
+def cycle_exists():
+    for start in range(1, N+1):
+        pos = start
+        for count in range(N):
+            pos = next_on_right[partner[pos]]
+        if pos != 0:
+            return True
+    return False
+
+def solve():
+    total = 0
+
+    for i in range(1, N+1):
+        if partner[i] == 0:
+            break
+    
+    if i == N:
+        if cycle_exists():
+            return 1
+        else:
+            return 0
+
+    for j in range(i+1, N+1):
+        if partner[j] == 0:
+            partner[i] = j
+            partner[j] = i
+            total += solve()
+            partner[i] = partner[j] = 0
+    return total
+    
+if __name__ == "__main__":
+    data = []
+
+    partner = [0] * 13
+    next_on_right = [0] * 13
+
+    X = [0]
+    Y = [0]
+
+    with open("wormhole.in", "r") as fin:
+        for line in fin:
+            data.append(line.strip())
+
+    N = int(data[0])
+
+    for i in range(1, len(data)):
+        a, b = map(int, data[i].split())
+        X.append(a)
+        Y.append(b)
+
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if X[j] > X[i] and Y[i] == Y[j]:
+                if next_on_right[i] == 0 or (X[j] - X[i] < X[next_on_right[i]] - X[i]):
+                    next_on_right[i] = j
+
+    pairs = solve()
+    with open("wormhole.out", "w") as fout:
+        fout.write(str(pairs) + "\n")
