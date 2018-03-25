ceb30927986a5298129b0b77fd759f8749834d18
xiaofeng <1205381116@qq.com>
2018-03-26 00:37:23 +0800

    update ariprog

--- ariprog/ariprog.py
+++ ariprog/ariprog.py
@@ -7,6 +7,10 @@ LANG: PYTHON2
 PROG: ariprog
 """
 
+"""
+原来判断一个元素在不在列表里面这么耗时的。
+"""
+
 #>>>L = [('b',6),('a',1),('c',3),('d',4)]
 #>>>L.sort(key=lambda x:x[1])
 #>>>L
@@ -26,35 +30,54 @@ def my_sort(lis):
 
 def get_S(M):
     S = set()
+    exist = [0] * (2*M*M+1)
     for i in range(M+1):
         for j in range(M+1):
             S.add(i*i + j*j)
-    return S
+            exist[i*i+j*j] = 1
+    return S, exist
 
-def get_sequences(N, S):
+def get_sequences(N, S, exist):
     sequences = []
     m = max(S)
-    for b in range(1, (m/2)+1):
-        for a in S[:-3]:
+    for b in range(1, (m/(N-1))+1):
+        for a in S[:-N+1]:
             count = 0
-            for n in range(N):
-                if (a+n*b) in S:
-                    count += 1
-            if count == N:
-                sequences.append([a,b])
+            if a + (N-1) * b > m:
+                break
+            sequences.append([a,b])
+            #for n in range(N):
+            #    count += 1
+            #    #if exist[a+n*b]:
+            #    #    count += 1
+            #    #else:
+            #    #    break
+            #if count == N:
+            #    sequences.append([a,b])
     return sequences
 
 if __name__ == "__main__":
+    import time
     with open("ariprog.in", "r") as fin:
         N = fin.readline().strip()
         N = int(N)
         M = fin.readline().strip()
         M = int(M)
 
-    S = get_S(M)
+    S, exist = get_S(M)
     S = list(S)
-    sequences = get_sequences(N, S)
+    S.sort()
+    print "b len ", (max(S)/(N-1))+1
+    print "S len ", len(S)
+    print "max S ", max(S)
+    #begin = time.time()
+    #sequences = get_sequences(N, S, exist)
+    ##print sequences
+    #end = time.time()
+    #print (end-begin)
+    
 
+    #sequences = get_sequences(N, S, exist)
     #with open("ariprog.out", "w") as fout:
     #    if not sequences:
     #        fout.write("NONE" + "\n")
